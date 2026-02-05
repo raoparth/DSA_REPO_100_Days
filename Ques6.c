f4027e3dc83f947f0020854709d0871ab43dfedd
Parth Yadav <yparth0379@gmail.com>
2026-02-05 08:51:55 +0530

    Add files via upload

---
+++ Ques6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    int arr[n - 1];
+    for(int i = 0; i < n - 1; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int totalSum = n * (n + 1) / 2;
+    int arrSum = 0;
+
+    for(int i = 0; i < n - 1; i++) {
+        arrSum += arr[i];
+    }
+
+    int missing = totalSum - arrSum;
+
+    printf("%d", missing);
+
+    return 0;
+}
